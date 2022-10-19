71f48b6d84550b35c3b66cd13fa4fb9cd5b0a859
Bokyeal <byby8992@naver.com>
2022-10-19 22:33:36 +0900

    drf api

--- ingredients/models.py
+++ ingredients/models.py
@@ -21,6 +21,8 @@ class Ingredient(TimeStampedModel):
     category = models.CharField(choices=INGREDIENT_CATEGORY_CHOICES, max_length=40)
     name = models.CharField(max_length=40, unique=True)
     expiry_date = models.PositiveIntegerField(blank=True, null=True)
+    # calorie_per_100g = models.PositiveIntegerField(default=0)
+    # price = models.PositiveIntegerField(default=0)
     preservation = models.CharField(max_length=80)
 
     def __str__(self) -> str:
