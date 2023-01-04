58d292ea1f586e40229d5030a9ab50f430611ffd
bewoody <woody@maycan.co.kr>
2023-01-04 12:07:00 +0900

    chore: ALLOWED_HOSTS 추가

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".execute-api.ap-northeast-2.amazonaws.com",
+]
 
 
 # Application definition
