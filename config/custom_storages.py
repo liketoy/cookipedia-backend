f86e68cd0df11333917383c18f9f5f59438e35f9
bewoody <woody@maycan.co.kr>
2023-01-05 18:19:05 +0900

    chore: django-storages 설정

--- config/custom_storages.py
+++ config/custom_storages.py
@@ -1,10 +1,15 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3Boto3Storage):
+class SecurityTokenWorkaroundS3Boto3Storage(S3Boto3Storage):
+    def _get_security_token(self):
+        return None
+
+
+class StaticStorage(SecurityTokenWorkaroundS3Boto3Storage):
     location = "static/"
     file_overwrite = False
 
 
-class UploadStorage(S3Boto3Storage):
+class UploadStorage(SecurityTokenWorkaroundS3Boto3Storage):
     location = "uploads/"
