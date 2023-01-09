e8b235e722b955c70540740678ad209588eeb2b0
bewoody <woody@maycan.co.kr>
2023-01-09 15:33:13 +0900

    chore: s3 token 수정

--- config/custom_storages.py
+++ config/custom_storages.py
@@ -1,15 +1,15 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class SecurityTokenWorkaroundS3Boto3Storage(S3Boto3Storage):
-    def _get_security_token(self):
-        return None
+# class SecurityTokenWorkaroundS3Boto3Storage(S3Boto3Storage):
+#     def _get_security_token(self):
+#         return None
 
 
-class StaticStorage(SecurityTokenWorkaroundS3Boto3Storage):
+class StaticStorage(S3Boto3Storage):
     location = "static/"
     file_overwrite = False
 
 
-class UploadStorage(SecurityTokenWorkaroundS3Boto3Storage):
+class UploadStorage(S3Boto3Storage):
     location = "uploads/"
