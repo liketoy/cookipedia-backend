7fe87ab6d7614c1095cf2623cda400f85a4841da
bewoody <woody@maycan.co.kr>
2023-01-09 15:30:51 +0900

    chore: 환경변수 변경

--- config/settings.py
+++ config/settings.py
@@ -192,8 +192,8 @@ if not DEBUG:
     REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
         "rest_framework.renderers.JSONRenderer",
     ]
-    AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
+    AWS_ACCESS_KEY_ID = env.str("AWS_S3_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = env.str("AWS_S3_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = "ap-northeast-2"
     AWS_DEFAULT_ACL = "public-read"
